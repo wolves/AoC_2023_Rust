efb56ccc0d96791320ef57229524c9015440a9d1
Wolves <cstingl@protonmail.com>
2024-04-05 14:44:11 -0400

    day-01: Add initial placeholder test

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("part1");
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    "142".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part1(
+            "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet",
+        );
+        assert_eq!(result, "142".to_string())
+    }
 }
