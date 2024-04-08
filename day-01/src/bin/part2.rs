0b8050453180f4372cf9ed283f9363a040de4cc1
Wolves <cstingl@protonmail.com>
2024-04-08 13:33:36 -0400

    day-01(part2): Add working solution for part2 along w/ rstest

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,3 +1,85 @@
 fn main() {
-    println!("part2");
+    let input = include_str!("./input1.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(input: &str) -> String {
+    let output = input.lines().map(process_line).sum::<u32>();
+
+    output.to_string()
+}
+
+fn process_line(line: &str) -> u32 {
+    let mut index = 0;
+    let line_iter = std::iter::from_fn(move || {
+        let reduced_line = &line[index..];
+
+        let result = if reduced_line.starts_with("one") {
+            Some('1')
+        } else if reduced_line.starts_with("two") {
+            Some('2')
+        } else if reduced_line.starts_with("three") {
+            Some('3')
+        } else if reduced_line.starts_with("four") {
+            Some('4')
+        } else if reduced_line.starts_with("five") {
+            Some('5')
+        } else if reduced_line.starts_with("six") {
+            Some('6')
+        } else if reduced_line.starts_with("seven") {
+            Some('7')
+        } else if reduced_line.starts_with("eight") {
+            Some('8')
+        } else if reduced_line.starts_with("nine") {
+            Some('9')
+        } else {
+            let result = reduced_line.chars().next();
+            result
+        };
+        index += 1;
+
+        result
+    });
+
+    let mut it = line_iter.filter_map(|character| character.to_digit(10));
+    let first = it.next().expect("should be a number");
+
+    match it.last() {
+        Some(num) => format!("{first}{num}"),
+        None => format!("{first}{first}"),
+    }
+    .parse::<u32>()
+    .expect("should be a valid number")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("two1nine", 29)]
+    #[case("eightwothree", 83)]
+    #[case("abcone2threexyz", 13)]
+    #[case("xtwone3four", 24)]
+    #[case("4nineeightseven2", 42)]
+    #[case("zoneight234", 14)]
+    #[case("7pqrstsixteen", 76)]
+    fn line_test(#[case] line: &str, #[case] expected: u32) {
+        assert_eq!(expected, process_line(line))
+    }
+
+    #[test]
+    fn it_works() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", part2(input))
+    }
 }
