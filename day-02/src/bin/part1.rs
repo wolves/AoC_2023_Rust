cbb28e9d47f1444ee005eb11dcd7880bd83cd0d2
Wolves <cstingl@protonmail.com>
2024-04-11 14:55:17 -0400

    day-02: build out nom parser for part1

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,13 +1,57 @@
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, alpha1, digit1, line_ending},
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
+
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part1(input);
     dbg!(output);
 }
 
-fn part1(input: &str) -> String {
-    let output = "42";
+#[derive(Debug)]
+struct Cube<'a> {
+    color: &'a str,
+    amount: u32,
+}
+#[derive(Debug)]
+struct Game<'a> {
+    id: &'a str,
+    rounds: Vec<Vec<Cube<'a>>>,
+}
 
-    output.to_string()
+// 4 red
+fn cube(input: &str) -> IResult<&str, Cube> {
+    let (input, (amount, color)) = separated_pair(complete::u32, tag(" "), alpha1)(input)?;
+    Ok((input, Cube { color, amount }))
+}
+
+// 3 blue, 4 red
+fn round(input: &str) -> IResult<&str, Vec<Cube>> {
+    let (input, cubes) = separated_list1(tag(", "), cube)(input)?;
+    Ok((input, cubes))
+}
+
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+fn game(input: &str) -> IResult<&str, Game> {
+    let (input, id) = preceded(tag("Game "), digit1)(input)?;
+    let (input, rounds) = preceded(tag(": "), separated_list1(tag("; "), round))(input)?;
+
+    Ok((input, Game { rounds, id }))
+}
+
+fn parse_games(input: &str) -> IResult<&str, Vec<Game>> {
+    let (input, games) = separated_list1(line_ending, game)(input)?;
+    Ok((input, games))
+}
+
+fn part1(input: &str) -> String {
+    let games = parse_games(input).expect("should parse");
+    dbg!(games);
+    "".to_string()
 }
 
 #[cfg(test)]
@@ -16,7 +60,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let input = "";
-        assert_eq!("42", part1(input))
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!("8", part1(input))
     }
 }
