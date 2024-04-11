084638bc2e21c62117f096eac966f1dec5d62a32
Wolves <cstingl@protonmail.com>
2024-04-11 16:34:05 -0400

    day-02(part1): Add initial working solution

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,6 +1,10 @@
+use std::collections::BTreeMap;
+
 use nom::{
     bytes::complete::tag,
-    character::complete::{self, alpha1, digit1, line_ending},
+    character::complete::{
+        self, alpha1, digit1, line_ending,
+    },
     multi::separated_list1,
     sequence::{preceded, separated_pair},
     IResult,
@@ -8,7 +12,7 @@ use nom::{
 
 fn main() {
     let input = include_str!("./input1.txt");
-    let output = part1(input);
+    let output = process(input);
     dbg!(output);
 }
 
@@ -23,35 +27,77 @@ struct Game<'a> {
     rounds: Vec<Vec<Cube<'a>>>,
 }
 
+impl<'a> Game<'a> {
+    fn valid_for_cube_set(
+        &self,
+        map: &BTreeMap<&str, u32>,
+    ) -> Option<u32> {
+        self.rounds
+            .iter()
+            .all(|round| {
+                round.iter().all(|shown_cube| {
+                    shown_cube.amount
+                        <= *map
+                            .get(shown_cube.color)
+                            .expect("a valid cube")
+                })
+            })
+            .then_some(
+                self.id.parse::<u32>().expect(
+                    "game id should be a parsable u32",
+                ),
+            )
+    }
+}
+
 // 4 red
 fn cube(input: &str) -> IResult<&str, Cube> {
-    let (input, (amount, color)) = separated_pair(complete::u32, tag(" "), alpha1)(input)?;
+    let (input, (amount, color)) =
+        separated_pair(complete::u32, tag(" "), alpha1)(
+            input,
+        )?;
     Ok((input, Cube { color, amount }))
 }
 
 // 3 blue, 4 red
 fn round(input: &str) -> IResult<&str, Vec<Cube>> {
-    let (input, cubes) = separated_list1(tag(", "), cube)(input)?;
+    let (input, cubes) =
+        separated_list1(tag(", "), cube)(input)?;
     Ok((input, cubes))
 }
 
-// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue;
+// 2 green
 fn game(input: &str) -> IResult<&str, Game> {
-    let (input, id) = preceded(tag("Game "), digit1)(input)?;
-    let (input, rounds) = preceded(tag(": "), separated_list1(tag("; "), round))(input)?;
+    let (input, id) =
+        preceded(tag("Game "), digit1)(input)?;
+    let (input, rounds) = preceded(
+        tag(": "),
+        separated_list1(tag("; "), round),
+    )(input)?;
 
     Ok((input, Game { rounds, id }))
 }
 
 fn parse_games(input: &str) -> IResult<&str, Vec<Game>> {
-    let (input, games) = separated_list1(line_ending, game)(input)?;
+    let (input, games) =
+        separated_list1(line_ending, game)(input)?;
     Ok((input, games))
 }
 
-fn part1(input: &str) -> String {
+fn process(input: &str) -> String {
+    let map = BTreeMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14),
+    ]);
     let games = parse_games(input).expect("should parse");
-    dbg!(games);
-    "".to_string()
+    games
+        .1
+        .iter()
+        .filter_map(|game| game.valid_for_cube_set(&map))
+        .sum::<u32>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -65,6 +111,6 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        assert_eq!("8", part1(input))
+        assert_eq!("8", process(input))
     }
 }
